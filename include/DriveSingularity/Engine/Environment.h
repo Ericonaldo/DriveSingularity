51a7ce61c2263b662f669cd8bc01faa88ad4b771
Ming Zhou <kornbergfresnel@outlook.com>
2019-10-06 13:43:48 +0800

    optimize: multiple threads

--- include/DriveSingularity/Engine/Environment.h
+++ include/DriveSingularity/Engine/Environment.h
@@ -41,75 +41,88 @@ struct FeedBack {
       : FeedBack(id, std::move(observation), reward, done, {}) {}
 };
 
-
 class Environment {
 public: // API
-  Environment(const roadmap::RoadMap& roadMap, const roadmap::Graph& roadGraph)
+  Environment(const roadmap::RoadMap &roadMap, const roadmap::Graph &roadGraph)
       : roadMap(std::make_shared<roadmap::RoadMap>(roadMap)),
         roadGraph(std::make_shared<roadmap::Graph>(roadGraph)) {
-      static VectorD videoAnchor1(0, 0);
-      static VectorD videoAnchor2(3000, 2400);
-      bool flag = true;
-      for (const auto &roadpair : roadMap.getRoads()) {
-        const auto & road = roadpair.second;
-        if (road->getRoadType() == roadmap::RoadType::Straight) {
-          auto straightRoad = std::dynamic_pointer_cast<roadmap::StraightRoad>(road);
-          if (flag) {
-            setAnchor1(straightRoad->getFromBorder().getEnd());
-            flag = false;
+    static VectorD videoAnchor1(0, 0);
+    static VectorD videoAnchor2(3000, 2400);
+    bool flag = true;
+    for (const auto &roadpair : roadMap.getRoads()) {
+      const auto &road = roadpair.second;
+      if (road->getRoadType() == roadmap::RoadType::Straight) {
+        auto straightRoad =
+            std::dynamic_pointer_cast<roadmap::StraightRoad>(road);
+        if (flag) {
+          setAnchor1(straightRoad->getFromBorder().getEnd());
+          flag = false;
+        }
+        updateAnchor1(straightRoad->getFromBorder().getEnd());
+        updateAnchor1(straightRoad->getFromBorder().getStart());
+        updateAnchor1(straightRoad->getToBorder().getEnd());
+        updateAnchor1(straightRoad->getToBorder().getStart());
+        updateAnchor2(straightRoad->getFromBorder().getEnd());
+        updateAnchor2(straightRoad->getFromBorder().getStart());
+        updateAnchor2(straightRoad->getToBorder().getEnd());
+        updateAnchor2(straightRoad->getToBorder().getStart());
+        for (size_t k = 0; k < straightRoad->getLanes().size(); ++k) {
+          const auto &lane = straightRoad->getLanes().at(k);
+          auto axi = straightRoad->getLaneAxis(k);
+          VectorD position;
+          double rotation;
+          if (lane.isReversed()) {
+            position = axi.getEnd();
+            rotation = (axi.getStart() - axi.getEnd()).getRotation();
+          } else {
+            position = axi.getStart();
+            rotation = (axi.getEnd() - axi.getStart()).getRotation();
           }
-          updateAnchor1(straightRoad->getFromBorder().getEnd());
-          updateAnchor1(straightRoad->getFromBorder().getStart());
-          updateAnchor1(straightRoad->getToBorder().getEnd());
-          updateAnchor1(straightRoad->getToBorder().getStart());
-          updateAnchor2(straightRoad->getFromBorder().getEnd());
-          updateAnchor2(straightRoad->getFromBorder().getStart());
-          updateAnchor2(straightRoad->getToBorder().getEnd());
-          updateAnchor2(straightRoad->getToBorder().getStart());
-          for (size_t k = 0; k < straightRoad->getLanes().size() ; ++k) {
-            const auto &lane = straightRoad->getLanes().at(k);
-            auto axi = straightRoad->getLaneAxis(k);
-            VectorD position;
-            double rotation;
-            if (lane.isReversed()) {
-              position = axi.getEnd();
-              rotation = (axi.getStart() - axi.getEnd()).getRotation();
-            } else {
-              position = axi.getStart();
-              rotation = (axi.getEnd() - axi.getStart()).getRotation();
-            }
-            //if (position.getX() < videoAnchor1.getX() || position.getY() < videoAnchor1.getY() ||
-              //  position.getX() > videoAnchor2.getX() || position.getY() > videoAnchor2.getY()) {
-              generators.emplace_back(std::make_pair(position, rotation));
-            //}
-          }
+          // if (position.getX() < videoAnchor1.getX() || position.getY() <
+          // videoAnchor1.getY() ||
+          //  position.getX() > videoAnchor2.getX() || position.getY() >
+          //  videoAnchor2.getY()) {
+          generators.emplace_back(std::make_pair(position, rotation));
+          //}
         }
       }
+    }
 
-    logger = Logger(); // create logger
-    agentActionType = control::ACTION_SPACE_TYPE::DISCRETE;  // default action space is discrete
+    logger = Logger();                                      // create logger
+    agentActionType = control::ACTION_SPACE_TYPE::DISCRETE; // default action
+                                                            // space is discrete
   }
-  
-  void setLogger(std::string& fpath) { logger.setFilePath(fpath);  logger.clear(); }
-  void record() { logger.log(epoch, vehicles); logger.log(roadMap->getNodes()); }
+
+  void setLogger(std::string &fpath) {
+    logger.setFilePath(fpath);
+    logger.clear();
+  }
+  void record() {
+    logger.log(epoch, vehicles);
+    logger.log(roadMap->getNodes());
+  }
   void saveRecord() { logger.writeToFile(); }
   void turnOnRecord() { recordOn = true; }
   void turnOffRecord() { recordOn = false; }
 
-  void loadVehicles(std::list<std::unordered_map<std::string, float>> &vehicles_config);
-	void initGenerator(size_t socialVehicleNum, std::pair<double, double> halfLengthRange,
-			std::pair<double, double> halfWidthRange, std::pair<double, double> velocityRange, std::pair<double, double> targetVelocityRange) {
-		setMaxVehicle(socialVehicleNum);
-		vehicleGenerator.setHalfLengthRange(halfLengthRange);
-		vehicleGenerator.setHalfWidthRange(halfWidthRange);
-		vehicleGenerator.setVelocityRange(velocityRange);
-		vehicleGenerator.setTargetVelocityRange(targetVelocityRange);
-	}
+  void loadVehicles(
+      std::list<std::unordered_map<std::string, float>> &vehicles_config);
+  void initGenerator(size_t socialVehicleNum,
+                     std::pair<double, double> halfLengthRange,
+                     std::pair<double, double> halfWidthRange,
+                     std::pair<double, double> velocityRange,
+                     std::pair<double, double> targetVelocityRange) {
+    setMaxVehicle(socialVehicleNum);
+    vehicleGenerator.setHalfLengthRange(halfLengthRange);
+    vehicleGenerator.setHalfWidthRange(halfWidthRange);
+    vehicleGenerator.setVelocityRange(velocityRange);
+    vehicleGenerator.setTargetVelocityRange(targetVelocityRange);
+  }
 
-  int loadGenerators(
-    const std::list<std::unordered_map<std::string, float>> &generator_config);
+  int loadGenerators(const std::list<std::unordered_map<std::string, float>>
+                         &generator_config);
 
-	void generateVehicles();
+  void generateVehicles();
 
   template <class VehicleType>
   VehicleId addVehicle(double halfLength, double halfWidth, double x, double y,
@@ -122,16 +135,18 @@ public: // API
                              std::size_t viewWidth = 84);
 
   void reset() {
-//    roadMap->reset();
-//    roadGraph->reset();
+    //    roadMap->reset();
+    //    roadGraph->reset();
     vehicles.clear();
     vehicleGrid.clear();
     vehicleGridPos.clear();
     eraseVehicleId.clear();
     agents.clear();
 
-    for (auto& kv: roadMap->getRoads()) kv.second->clear();
-    for (auto& kv: roadMap->getNodes()) kv.second->clear();
+    for (auto &kv : roadMap->getRoads())
+      kv.second->clear();
+    for (auto &kv : roadMap->getNodes())
+      kv.second->clear();
 
     control::VEHICLE_ID_MANAGER = 0;
 
@@ -141,14 +156,14 @@ public: // API
   void addListener(VehicleId vehicleId, std::vector<EventFlag::Type> events) {
     for (auto e : events) {
       vehicles.at(vehicleId)->setEventListening(e);
-//      std::cout << "[DEBUG] set event: " << e << std::endl;
+      //      std::cout << "[DEBUG] set event: " << e << std::endl;
     }
   }
 
   std::vector<FeedBack>
   step(const std::unordered_map<VehicleId, std::size_t> &action);
 
-//  std::unordered_map<VehicleId, std::pair<int, int>> observation_space;
+  //  std::unordered_map<VehicleId, std::pair<int, int>> observation_space;
   std::unordered_map<VehicleId, int> action_space;
 
 public: // c++ public callable
@@ -199,9 +214,7 @@ public: // inner callable
 
   void updateAnchor2(const VectorD &anchor2);
 
-  VehicleGenerator &getVehicleGenerator(){
-    return vehicleGenerator;
-  }
+  VehicleGenerator &getVehicleGenerator() { return vehicleGenerator; }
 
 private:
   bool checkCollision(VehicleId vehicleId) const;
@@ -237,9 +250,7 @@ private:
    * @param window
    * @return list<pair<road line, whether the line is continuous>>
    */
-  std::list<std::pair<Segment, bool>>
-  getInvolvedRoadLines(const Obb &window);
-
+  std::list<std::pair<Segment, bool>> getInvolvedRoadLines(const Obb &window);
 
 public:
   void setMaxVehicle(size_t maxVehicle);
@@ -330,11 +341,12 @@ Environment::addVehicle(const std::unordered_map<std::string, float> &attrs) {
 namespace ds {
 namespace engine {
 
-Observation drawObservation(
-    const VectorD &center, std::size_t rangeLength, std::size_t rangeWidth,
-    const std::list<std::shared_ptr<control::VehicleController>>
-        &adjacentVehicles,
-    const std::list<std::pair<Segment, bool>> &involvedLines);
+Observation
+drawObservation(const VectorD &center, std::size_t rangeLength,
+                std::size_t rangeWidth,
+                const std::list<std::shared_ptr<control::VehicleController>>
+                    &adjacentVehicles,
+                const std::list<std::pair<Segment, bool>> &involvedLines);
 
 void printObservation(const Observation &observation);
 
